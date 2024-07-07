076155614005f0cc8c903bdf1d7653ea8a2e5cc2
thailanelopes <thailane111122@gmail.com>
2024-07-07 12:38:42 -0300

    subindo a aplicação

---
+++ todolist/src/main/java/br/com/thailanelopes/todolist/controller/PrimeiraRotaController.java
@@ -0,0 +1,20 @@
+package br.com.thailanelopes.todolist.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiraRotaController {
+
+    /**
+     * GET - Buscar uma informação
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar um dado/informação
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte do dado/informação
+     * 
+     */
+
+    public String primeiraMensagem() {
+       return "Funcionou";
+    }
+}
