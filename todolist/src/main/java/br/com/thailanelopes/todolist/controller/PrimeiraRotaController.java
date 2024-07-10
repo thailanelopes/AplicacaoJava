1bd26f07a3959622b4bbff205a25955ff948ae9b
thailanelopes <thailane111122@gmail.com>
2024-07-09 21:26:37 -0300

    correção do README

--- todolist/src/main/java/br/com/thailanelopes/todolist/controller/PrimeiraRotaController.java
+++ todolist/src/main/java/br/com/thailanelopes/todolist/controller/PrimeiraRotaController.java
@@ -1,20 +1,21 @@
-package br.com.thailanelopes.todolist.controller;
+    package br.com.thailanelopes.todolist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+    import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PrimeiraRotaController {
+    @RestController
+    public class PrimeiraRotaController {
 
-    /**
-     * GET - Buscar uma informação
-     * POST - Adicionar um dado/informação
-     * PUT - Alterar um dado/informação
-     * DELETE - Remover um dado
-     * PATCH - Alterar somente uma parte do dado/informação
-     * 
-     */
+        /**
+         * GET - Buscar uma informação
+         * POST - Adicionar um dado/informação
+         * PUT - Alterar um dado/informação
+         * DELETE - Remover um dado
+         * PATCH - Alterar somente uma parte do dado/informação
+         * 
+         */
 
-    public String primeiraMensagem() {
-       return "Funcionou";
+        public String primeiraMensagem() {
+        return "Funcionou";
+        }
+        
     }
-}
